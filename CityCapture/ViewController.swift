e3bd814094e16ee4c62b45db0107f8b25ecc76e0
Kelly-Chui <optioneldar@gmail.com>
2023-04-27 16:05:58 +0900

    Initial Commit

---
+++ CityCapture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CityCapture
+//
+//  Created by KellyChui on 2023/04/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
